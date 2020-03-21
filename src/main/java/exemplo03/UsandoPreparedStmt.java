986549c7b425ac61da60d445409496122cc30432
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2020-03-21 18:19:41 -0300

    exemplo 03 refatorado

--- src/main/java/exemplo03/UsandoPreparedStmt.java
+++ src/main/java/exemplo03/UsandoPreparedStmt.java
@@ -13,94 +13,98 @@ import java.sql.SQLException;
  */
 public class UsandoPreparedStmt {
 
+    public String listarPessoas() {
+        StringBuilder sb = new StringBuilder();
 
-    /**
-     * Método principal
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        UsandoPreparedStmt exer03 = new UsandoPreparedStmt();
-
-//        exer03.listarDadosDeUmaPessoa(1);
-//        exer03.atualizaEmail(1, "novo@email.com");
-
-        exer03.listarPessoas();
-
-        exer03.listarPessoas();
-    }
-
-    public void listarPessoas() {
         String query = "SELECT * FROM Pessoa";
 
         try (Connection conexao = ConnectionFactory.getDBConnection();
-             PreparedStatement stmt = conexao.prepareStatement(query)) {
+             PreparedStatement stmt = conexao.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
 
-            ResultSet rs = stmt.executeQuery();
+            if (!rs.next()) {
+                sb.append("\nNenhuma pessoa cadastrada no banco\n");
+            } else {
+                // Formantado a saída para melhorar a apresentação para o usuário
+                sb.append("---------------------------------------------------------------------------------\n");
+                sb.append(String.format("|%-5s|%-25s|%-10s|%-10s|%-25s|\n", "ID", "Nome", "Peso", "Altura", "Email"));
+                sb.append("---------------------------------------------------------------------------------\n");
 
-            System.out.println("---------------------------------------------------------------------------------");
-            System.out.println(String.format("|%-5s|%-25s|%-10s|%-10s|%-25s|", "ID", "Nome", "Peso", "Altura", "Email"));
-            System.out.println("---------------------------------------------------------------------------------");
-            while (rs.next()) {
-                System.out.println(String.format("|%-5d|%-25s|%-10.2f|%-10d|%-25s|",
-                        rs.getInt("idPessoa"),
-                        rs.getString("Nome"),
-                        rs.getDouble("peso"),
-                        rs.getInt("altura"),
-                        rs.getString("email")));
+                // Percorrendo todas as linhas resultantes da consulta SQL
+                do {
+                    sb.append(String.format("|%-5d|%-25s|%-10.2f|%-10d|%-25s|\n",//formatando String para melhorar apresentação
+                            rs.getInt("idPessoa"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getString("Nome"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getDouble("peso"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getInt("altura"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getString("email")));
+                } while (rs.next());
+                sb.append("---------------------------------------------------------------------------------");
             }
-            System.out.println("---------------------------------------------------------------------------------");
-            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return sb.toString();
     }
 
-    public void listarDadosDeUmaPessoa(int idPessoa) {
+    public String listarDadosDeUmaPessoa(int idPessoa) {
+        StringBuilder sb = new StringBuilder();
+
+        // Ao invés de concatenar o parâmetro idPessoa, coloca-se uma interrogação
+        // Essa será preenchida logo abaixo
         String query = "SELECT * FROM Pessoa WHERE idPessoa = ?";
 
         try (Connection conexao = ConnectionFactory.getDBConnection();
              PreparedStatement stmt = conexao.prepareStatement(query)) {
 
-
+            // Preenchendo o primeiro campo ? na String query criada acima
             stmt.setInt(1, idPessoa);
+
             ResultSet rs = stmt.executeQuery();
 
-            System.out.println("---------------------------------------------------------------------------------");
-            System.out.println(String.format("|%-5s|%-25s|%-10s|%-10s|%-25s|", "ID", "Nome", "Peso", "Altura", "Email"));
-            System.out.println("---------------------------------------------------------------------------------");
-            while (rs.next()) {
-                System.out.println(String.format("|%-5d|%-25s|%-10.2f|%-10d|%-25s|",
-                        rs.getInt("idPessoa"),
-                        rs.getString("Nome"),
-                        rs.getDouble("peso"),
-                        rs.getInt("altura"),
-                        rs.getString("email")));
+            if (rs.next() == false) {
+                sb.append("\nNenhuma pessoa cadastrada no banco\n");
+            } else {
+                // Formantado a saída para melhorar a apresentação para o usuário
+                sb.append("---------------------------------------------------------------------------------\n");
+                sb.append(String.format("|%-5s|%-25s|%-10s|%-10s|%-25s|\n", "ID", "Nome", "Peso", "Altura", "Email"));
+                sb.append("---------------------------------------------------------------------------------\n");
+
+                // Percorrendo todas as linhas resultantes da consulta SQL
+                do {
+                    sb.append(String.format("|%-5d|%-25s|%-10.2f|%-10d|%-25s|\n",//formatando String para melhorar apresentação
+                            rs.getInt("idPessoa"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getString("Nome"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getDouble("peso"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getInt("altura"),// é necessário saber o nome da coluna e o domínio dos dados
+                            rs.getString("email")));
+                } while (rs.next());
+                sb.append("---------------------------------------------------------------------------------");
             }
-            System.out.println("---------------------------------------------------------------------------------");
             rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return sb.toString();
     }
 
-    public void atualizaEmail(int idPessoa, String email) {
+    public int atualizaEmail(int idPessoa, String email) {
+        int totalLinhasModificadas = 0;
+
+        // Os campos com interrogação serão preenchidos abaixo
         String query = "UPDATE Pessoa SET Email = ?  WHERE idPessoa = ?";
 
         try (Connection conexao = ConnectionFactory.getDBConnection();
              PreparedStatement stmt = conexao.prepareStatement(query)) {
 
-
             stmt.setString(1, email);
             stmt.setInt(2, idPessoa);
 
-            int totalLinhasModificadas = stmt.executeUpdate();
-            System.out.println("Total de registros modificados: " + totalLinhasModificadas);
-
+            totalLinhasModificadas = stmt.executeUpdate();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return totalLinhasModificadas;
     }
 }
