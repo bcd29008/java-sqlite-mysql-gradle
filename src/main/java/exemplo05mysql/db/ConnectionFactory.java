02a65f34c2e7d4f926e8363151ad27455b18b740
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2020-12-16 18:54:30 -0300

    fazendo uso de arquivo properties para guardar informações sobre a conexão com o servidor MySQL

--- src/main/java/exemplo05mysql/db/ConnectionFactory.java
+++ src/main/java/exemplo05mysql/db/ConnectionFactory.java
@@ -1,5 +1,7 @@
 package exemplo05mysql.db;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,7 +11,7 @@ import java.util.logging.Logger;
 
 /**
  * <p>
- * O arquivo resources/lab01-dml-ddl.sql contém as instruções DML e DDL para criação
+ * O arquivo resources/lab01-mysql-dml-ddl.sql contém as instruções DML e DDL para criação
  * do banco de dados necessário para esse exemplo. Crie um esquema em uma instalação
  * MySQL / MariaDB e importe o conteúdo desse arquivo.
  * <p>
@@ -20,7 +22,24 @@ import java.util.logging.Logger;
  */
 public abstract class ConnectionFactory {
 
+    private static final String DB_PROPERTIES_FILE = "database.properties";
+
     private static Connection cnx;
+
+    /**
+     * Para carregar o arquivo properties
+     * @return
+     */
+    private static InputStream getInputStream(){
+        // Para pegar o arquivo quando estiver dentro um .jar
+        InputStream is = ConnectionFactory.class.getResourceAsStream("/resources/"+DB_PROPERTIES_FILE);
+
+        // Para pegar o arquivo quando estiver executando pela IDE
+        if (is == null){
+            is = ConnectionFactory.class.getClassLoader().getResourceAsStream(DB_PROPERTIES_FILE);
+        }
+        return is;
+    }
 
     /**
      * Faz a conexão em um banco de dados MySQL e retorna o objeto Connection
@@ -29,21 +48,23 @@ public abstract class ConnectionFactory {
      */
     public static synchronized Connection getDBConnection() {
         Properties properties = new Properties();
-
-        properties.setProperty("user", "nome-do-usuario");
-        properties.setProperty("password", "senha-do-usuario");
-        properties.setProperty("useSSL", "false");
-
-        String host = "127.0.0.1";
-        String port = "3306";
-        String dbname = "lab01";
-
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + dbname;
-
         try {
+            // carregando as propriedades user, password e useSSL do arquivo database.properties
+            properties.load(getInputStream());
+
+            // obtendo valores para host, port e dbname do arquivo properties
+            String host = properties.getProperty("host");
+            String port = properties.getProperty("port");
+            String dbname = properties.getProperty("database");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + dbname;
+
             cnx = DriverManager.getConnection(url, properties);
+
+        } catch (IOException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, "arquivo properties não encontrado", ex);
         } catch (SQLException ex) {
-            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, "erro com instrução SQL", ex);
         }
 
         return cnx;
